87e8bb1d62093ffde61a5777b08e3316d57435b9
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-28 14:36:34 +0300

    type annottion

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -56,7 +56,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Returns a connector to the database
     (mysql.connector.connection.MySQLConnection object)
