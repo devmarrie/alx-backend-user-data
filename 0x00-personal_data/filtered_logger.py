73baea761c5336732a30e2791762c9de1a161f4a
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-28 11:29:50 +0300

    editing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,12 +6,10 @@ import re
 from typing import List
 
 
-def filter_datum(fields:List[str], redaction:str, 
-        message:str, separator:str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
-    returns the log message obfuscated: 
+    returns the log message obfuscated:
     """
     PII = re.compile(fr'\b({"|".join(fields)})=.*?({separator})')
     return re.sub(PII, fr'\1={redaction}\2', message)
-
-
