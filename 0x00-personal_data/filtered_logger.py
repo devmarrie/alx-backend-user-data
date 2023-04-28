f7a0bab194265bfd288d384a1afe7f2702094abd
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-28 11:03:49 +0300

    removing the test for one

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,11 +13,6 @@ def filter_datum(
     returns the log message obfuscated: 
     """
     PPI = re.compile(fr'\b({"|".join(fields)})=.*?({separator})')
-    print(PPI.sub(fr'\1={redaction}\2', message))
-        
-    
-"""if __name__ == "__main__":
-    f = ["password", "date_of_birth"]
-    m = "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;"
-    filter_datum(f,'xxx', m, ';')"""
+    return PPI.sub(fr'\1={redaction}\2', message)
+
 
