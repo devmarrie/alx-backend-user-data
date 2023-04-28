ed2abe0ae5f14e7da1fd9fa8fcb2ac72047a1750
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-28 11:01:11 +0300

    filtered trial

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,21 +7,17 @@ from typing import List
 
 
 def filter_datum(
-        fields:List[str], 
-        redaction:str, 
-        message:str, 
-        separator:str
-        ) -> str:
-    regex = re.compile(fr'\b({"|".join(fields)})=.*?({separator})')
-    print(regex.sub(fr'\1={redaction}\2', message)) 
-    """pii = re.compile(r'\b([A-Za-z]+\b|\d{2}/\d{2}/\d{4}\b)')
-    for i in fields:
-        message[i] = pii.sub(redaction, message[i])
-    return message"""
+        fields:List[str], redaction:str, 
+        message:str, separator:str) -> str:
+    """
+    returns the log message obfuscated: 
+    """
+    PPI = re.compile(fr'\b({"|".join(fields)})=.*?({separator})')
+    print(PPI.sub(fr'\1={redaction}\2', message))
         
     
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     f = ["password", "date_of_birth"]
     m = "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;"
-    filter_datum(f,'xxx', m, ';')
+    filter_datum(f,'xxx', m, ';')"""
 
