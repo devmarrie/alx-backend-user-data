4f06892a4c9bc81cb6f9ca799c03ecca10f57bac
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-28 12:40:52 +0300

    adding a class and formating it

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Regex-ing
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -13,3 +14,25 @@ def filter_datum(fields: List[str], redaction: str,
     """
     PII = re.compile(fr'\b({"|".join(fields)})=.*?({separator})')
     return re.sub(PII, fr'\1={redaction}\2', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        calls the format method of the parent class LogRecord
+        """
+        message = super(RedactingFormatter, self).format(record)
+        new = filter_datum(self.fields, self.REDACTION,
+                           message, self.SEPARATOR)
+        return new
