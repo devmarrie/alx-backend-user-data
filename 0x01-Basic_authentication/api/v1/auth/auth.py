2b0632049184b398a8462680c842ea1a61a2b194
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-03 00:56:44 +0300

    template class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+template for all authentication system
+you will implement
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        To be updated
+        """
+        return False
+    
+    def authorization_header(self, request=None):
+        """
+        To be updated
+        """
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        To be updated
+        """
+        return None
