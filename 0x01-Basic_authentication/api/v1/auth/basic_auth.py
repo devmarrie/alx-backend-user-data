c1b6b4c75cdc2d9ad9c7510f595625452fb7f5d7
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-03 13:01:43 +0300

    extract base

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Inherits from Auth
 """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -10,4 +11,17 @@ class BasicAuth(Auth):
     This class will represent the
     basic auth implementation of the user
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extract the base64 code
+        from the authorization header
+        """
+        if authorization_header is None:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+        elif not authorization_header.startswith("Basic "):
+            return None
+        credentials = authorization_header.split(" ")[-1]
+        return credentials
