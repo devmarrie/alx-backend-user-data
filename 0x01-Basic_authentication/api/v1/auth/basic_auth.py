e7a51dba396b37259b807e0f7a1acfbfee45f3ac
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-03 13:28:21 +0300

    decoding to find a value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,3 +25,21 @@ class BasicAuth(Auth):
             return None
         credentials = authorization_header.split(" ")[-1]
         return credentials
+    
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str: 
+        """
+        Returns the decoded value of a
+        base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            tobytes = base64_authorization_header.encode('utf-8')
+            tob64 = base64.b64decode(tobytes)
+            return tob64.decode('utf-8')
+        except Exception:
+            return None
