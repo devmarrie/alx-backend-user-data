035dc3d95fd3116d48cabf23bc942a8fc351857b
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-03 14:36:33 +0300

    checking the validity

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,8 @@
 Inherits from Auth
 """
 from .auth import Auth
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 import base64
 
 
@@ -61,3 +62,26 @@ class BasicAuth(Auth):
         email = splited[0]
         password = splited[1]
         return (email, password)
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str
+                                     ) -> TypeVar('User'):
+        """
+        returns the User instance
+        based on his email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({"email": user_email})
+            if not users or users == []:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+            return None
+        except Exception:
+            return None
