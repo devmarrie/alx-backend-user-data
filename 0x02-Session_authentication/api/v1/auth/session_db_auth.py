1221d1e569c5182f09ef032d5194cf978dbf98f2
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-05 23:04:41 +0300

    authenticating db user

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Session db authentiction
+"""
+from .session_exp_auth import SessionExpAuth
+
+
+class SessionDBAuth(SessionExpAuth):
+    """
+    move them to db
+    """
+    def create_session(self, user_id=None):
+        """
+        reates and stores new instance of
+        UserSession and returns the Session ID
+        """
+        sess_id = self.create_session(user_id)
+        return sess_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """
+        returns the User ID by requesting UserSession
+        """
+        user_id = self.user_id_for_session_id(session_id)
+        return user_id
+
+    def destroy_session(self, request=None):
+        """
+        destroy the database session
+        """
+        destroy = self.destroy_session(request)
+        return destroy
