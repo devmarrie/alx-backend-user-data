23266ef3ea03edd9f2e2bb786d8b0a6e8a379d78
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-05 16:30:07 +0300

    my try

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,12 +39,16 @@ def load_user():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/'
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, remove):
             if auth.authorization_header(request) is None:
                 return abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 return abort(403, description="Forbidden")
+            if auth.authorization_header(request) is None:
+                if auth.session_cookie(request) is None:
+                    return abort(401, description='Unauthorized')
 
 
 @app.errorhandler(404)
