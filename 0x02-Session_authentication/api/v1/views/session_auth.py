6d2bde925bb362d2a988f7d4d3f310163987b671
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-05 18:41:30 +0300

    session info

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+session authentication views
+"""
+from api.v1.views import app_views
+from models.user import User
+from flask import request, jsonify, make_response
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def user_session():
+    """
+    Fetch email and password from a html form using the post mthd
+    searches for a user based on their email
+    Returns the user and their session
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    session_name = os.getenv('SESSION_NAME')
+    if email is None or len(email) == 0:
+        response = make_response(jsonify('{ "error": "email missing" }'), 400)
+        return response
+    if password is None or len(password) == 0:
+        presponse = make_response(
+            jsonify('{ "error": "password missing" }'), 400)
+        return presponse
+    try:
+        users = User.search({"email": email})
+        if not users or users == []:
+            return make_response(
+                jsonify('{ "error": "no user found for this email" }'), 404)
+        for user in users:
+            if not user.is_valid_password(password):
+                return make_response(jsonify(
+                    '{ "error": "wrong password" } '), 401)
+            from api.v1.app import auth
+            sess_id = auth.create_session(user.id)
+            res = make_response(jsonify(user.to_json()))
+            res.set_cookie(session_name, sess_id)
+            return res
+    except Exception:
+        return None
