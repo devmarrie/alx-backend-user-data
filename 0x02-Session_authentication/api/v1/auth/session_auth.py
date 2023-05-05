e1407d696fc4b983ac1b9aae02b8a857abb44469
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-05 14:35:07 +0300

    session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Session authentication
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -10,4 +11,17 @@ class SessionAuth(Auth):
     using session auth
     instead of basic auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id
+        """
+        if user_id is None:
+            return None
+        elif not isinstance(user_id, str):
+            return None
+        else:
+            id = str(uuid.uuid4())
+            self.user_id_by_session_id[id] = user_id
+            return id
